1f15bdcc177d5f48915236af407fd4e0b0e2984f
Itcast.Mall <46169@DESKTOP-0TS5Q8I>
2016-05-02 22:54:37 +0800

    实现验证码功能以及页面元素的一些修改

---
+++ Itcast.Mall/Controllers/CaptchaHelperController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Itcast.Mall.Utility;
+namespace Itcast.Mall.Controllers
+{
+    public class CaptchaHelperController : Controller
+    {
+        // GET: CaptchaHelper
+        public void GetCaptch()
+        {
+            var captch = CaptchaHelper.DrawImage(CaptchaHelper.CreateRandomCode(4));
+            Response.AddHeader("Content-Type", "img/jpeg");
+            Response.BinaryWrite(captch);
+            Response.End();
+        }
+    }
+}
---
+++ Itcast.Mall/Models/Register.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itcast.Models
+{
+    public class Register
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Login
+        /// </summary>
+        public string Login { get; set; }
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Mobile
+        /// </summary>
+        public string Mobile { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Nickname
+        /// </summary>
+        public string Nickname { get; set; }
+        /// <summary>
+        /// Status
+        /// </summary>
+        public int Status { get; set; }
+
+    }
+}
