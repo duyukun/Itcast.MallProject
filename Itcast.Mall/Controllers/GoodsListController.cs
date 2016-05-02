c6b2cf30a8384554dbee1ca0955d2fef9d9aa0a6
Itcast.Mall <46169@DESKTOP-0TS5Q8I>
2016-05-02 16:45:40 +0800

    页面布局

---
+++ Itcast.Mall/Controllers/GoodsListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Itcast.Mall.Controllers
+{
+    public class GoodsListController : Controller
+    {
+        // GET: GoodsList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Itcast.Mall/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Itcast.Mall.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegsterStepOne()
+        {
+            return View();
+        }
+
+        public ActionResult RegsterStepTwo()
+        {
+            return View();
+        }
+    }
+}
